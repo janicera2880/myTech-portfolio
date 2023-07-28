1dfbff77e023f13db00a4a1abb97ff7f98a75e53
Janice <janiceralecha@gmail.com>
2023-07-27 22:28:04 -0500

    Created About component

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import bannerLogo from '../Images/bannerlogo.gif';
+
+const About = () => {
+  return (
+    <img
+      src={bannerLogo}
+      width="700"
+      height="200"
+      alt="logo"
+      className="banner"
+    />
+  );
+};
+
+export default About;
